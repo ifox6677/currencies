8acc1a9d78fe09d09dbd7d413fd5fd6835fba709
ifox6677 <zhangjq0908@outlook.com>
2025-11-18 04:31:35 +0800

    Update Language.kt

--- app/src/main/kotlin/de/salomax/currencies/model/Language.kt
+++ app/src/main/kotlin/de/salomax/currencies/model/Language.kt
@@ -9,38 +9,8 @@ enum class Language(
     private val nameLocalized: Int
 ) {
     SYSTEM("system", null, R.string.system_default),
-    IN("in", "Bahasa Indonesia", R.string.language_in),
-    CA("ca", "Català", R.string.language_ca),
-    CS("cs", "Čeština", R.string.language_cs),
-    DA("da", "Dansk", R.string.language_da),
-    DE("de", "Deutsch", R.string.language_de),
-    ET("et", "Eesti", R.string.language_et),
-    EN("en", "English", R.string.language_en),
-    EN_GB("en_GB", "English (United Kingdom)", R.string.language_en_GB),
-    EO("eo", "Esperanto", R.string.language_eo),
-    ES("es", "Español", R.string.language_es),
     FR("fr", "Français", R.string.language_fr),
-    HR("hr", "Hrvatski", R.string.language_hr),
-    IS("is", "Íslenska", R.string.language_is),
-    IT("it", "Italiano", R.string.language_it),
-    HU("hu", "Magyar", R.string.language_hu),
-    NL("nl", "Nederlands", R.string.language_nl),
-    NB("nb", "Norsk", R.string.language_nb),
-    PL("pl", "Polski", R.string.language_pl),
-    PT_BR("pt_BR", "Português (Brasil)", R.string.language_pt_BR),
-    SV("sv", "Svenska", R.string.language_sv),
-    VI("vi", "Tiếng Việt", R.string.language_vi),
-    TR("tr", "Türkçe", R.string.language_tr),
-    EL("el", "Ελληνικά", R.string.language_el),
-    BG("bg", "Български", R.string.language_bg),
-    RU("ru", "Русский", R.string.language_ru),
-    UK("uk", "Українська", R.string.language_uk),
-    IW("iw", "עִבְרִית", R.string.language_iw),
-    AR("ar", "اَلْعَرَبِيَّة", R.string.language_ar),
-    FA("fa", "فارسی", R.string.language_fa),
-    BN("bn", "বাংলা", R.string.language_bn),
-    ZH_CN("zh_CN", "简体中文", R.string.language_zh_CN),
-    ZH_TW("zh_TW", "正體中文", R.string.language_zh_TW);
+    ZH_CN("zh_CN", "简体中文", R.string.language_zh_CN);
 
     companion object {
         private val isoMapping: Map<String, Language> = entries.associateBy(Language::iso)
